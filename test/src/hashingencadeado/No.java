492c5fd69e916998b64c256742c8099872cd5f56
Bruno <64101776+brunoakl@users.noreply.github.com>
2023-08-21 10:10:09 -0400

    Add files via upload

---
+++ test/src/hashingencadeado/No.java
@@ -0,0 +1,29 @@
+package hashingencadeado;
+
+public class No {
+    private String chave;
+    private No prox = null;
+    
+    
+    public String getChave() {
+        return chave;
+    }
+
+    public void setChave(String chave) {
+        this.chave = chave;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
+
+
+
+        
+    
+    
+}
